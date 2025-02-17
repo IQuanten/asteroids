ec64daf00763e0e9f6a64ea406805920ba205a73
Ian Quanten <quantenian@live.be>
2025-02-17 20:58:54 +0100

    feat: Player is now drawn as a triangle

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pygame
-from constants import *
+from Constants import *
+from Player import Player 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    game_speed = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
-        screen.fill(color=0)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        game_speed.tick(60)
+         # limit the framerate to 60 FPS
+        dt = game_speed.tick(60) / 1000
             
 if __name__ == "__main__":
     main()
